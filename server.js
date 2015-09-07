a46c9071b34c7eb9e5e9506e7d7d5e008875c544
Kristof Kotai <kristof.kotai@iii.co.uk>
2015-09-07 19:59:08 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
+
+var server = app.listen(1234, function(){
+  console.log('Server started: ', server.address());
+});
