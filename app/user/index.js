0efa51a3f24741389c50ac24ea700f1a5796b3fa
Kristof Kotai <kristof.kotai@iii.co.uk>
2015-10-16 20:19:22 +0000

    Basic user routes added

---
+++ app/user/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'get': require('get.js'),
+  'getAll': require('getAll.js'),
+  'post': require('post.js'),
+  'update': require('update.js'),
+  'delete': require('delete.js')
+};
